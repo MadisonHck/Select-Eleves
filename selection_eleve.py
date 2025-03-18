1f7134b46fdf6ba9da182c51c9550bd30ec5bb9b
SimonLoukian <simonlk970@gmail.com>
2025-03-18 16:18:14 +0100

    évolution de selection_eleve.py

--- selection_eleve.py
+++ selection_eleve.py
@@ -1,9 +1,7 @@
+#!/usr/bin/env python3 -i
 from random import choice
-from liste_etudiant.py import liste_des_eleves
-# cette fonction a pour but de séléctionner un nom aléatoire dans une liste de nom donner
+
+# cette fonction a pour but de sélectionner aléatoirement une chaîne dans une liste de chaîne
 def selection_aleatoire(liste_eleve):
-    choix_aleatoire = choice(liste_eleve)
-    
+    choix_aleatoire = choice(liste_eleve)    
     return choix_aleatoire
-
-
