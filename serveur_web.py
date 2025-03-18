7ba7b75798d7fa9501ec850a2f628e69e83f1be8
R-udy <demarerudy@gmail.com>
2025-03-18 16:20:13 +0100

    Ajout d'une page statistiques

--- serveur_web.py
+++ serveur_web.py
@@ -1,9 +1,13 @@
 from bottle import route, run
-from document import generation_plaintext
+from document import generation_plaintext, generation_html, stats_html
 
 @route('/')
 def tirage():
     return generation_plaintext()
+
+@route('/statistique')
+def statistique():
+    return stats_html()
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8080)
